f29f56412cb579f1a94e710ac968a4df39b2ade2
anton-boyanov <hmmmmm75@yahoo.com>
2019-11-17 05:36:34 +0200

    apm

--- iam/main.tf
+++ iam/main.tf
@@ -5,14 +5,14 @@ resource "aws_iam_instance_profile" "ecs_instance_profile" {
     role = "${aws_iam_role.ecs_instance_role.name}"
 }
 resource "aws_iam_role" "ecs_instance_role" {
-    name = "endava_ecs_instance_role"
+    name = "apm_ecs_instance_role"
     assume_role_policy = "${data.template_file.ecs_instance_role.rendered}"
 }
 data "template_file" "ecs_instance_role" {
     template= "${file("iam/ecs_instance_role.tpl")}"
 }
 resource "aws_iam_role_policy" "ecs_instance_role_policy" {
-    name = "endava_ecs_instance_role_policy"
+    name = "apm_ecs_instance_role_policy"
     role = "${aws_iam_role.ecs_instance_role.id}"
     policy = "${data.template_file.ecs_instance_role_policy.rendered}"
 }
@@ -20,14 +20,14 @@ data "template_file" "ecs_instance_role_policy" {
     template= "${file("iam/ecs_instance_role_policy.tpl")}"
 }
 resource "aws_iam_role" "ecs_service_role" {
-    name = "endava_ecs_service_role"
+    name = "apm_ecs_service_role"
     assume_role_policy = "${data.template_file.ecs_service_role.rendered}"
 }
 data "template_file" "ecs_service_role" {
     template= "${file("iam/ecs_service_role.tpl")}"
 }
 resource "aws_iam_role_policy" "ecs_service_role_policy" {
-    name = "endava_ecs_service_role"
+    name = "apm_ecs_service_role"
     role = "${aws_iam_role.ecs_service_role.id}"
     policy = "${data.template_file.ecs_service_role_policy.rendered}"
 }
