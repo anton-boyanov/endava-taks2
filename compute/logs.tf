796c188feeaefdc46762fa0143ce8101fc5e18ed
anton-boyanov <hmmmmm75@yahoo.com>
2019-11-17 07:22:24 +0200

    Update logs.tf

--- compute/logs.tf
+++ compute/logs.tf
@@ -5,7 +5,7 @@ resource "aws_cloudwatch_log_group" "endava_log_group" {
     name              = "/ecs/endava-app"
     retention_in_days = 30
 
-    tags {
+    tags = {
         Name = "endava-log-group"
     }
 }
