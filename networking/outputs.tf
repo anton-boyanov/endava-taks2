8376ce8e7758bd831e8811b54c8dd087bbd989fb
anton-boyanov <hmmmmm75@yahoo.com>
2019-11-17 05:28:28 +0200

    Update outputs.tf
    apm

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -1,25 +1,25 @@
 #---networking/outputs.tf
 
-output "endava_public_subnets" {
-    value = "${aws_subnet.endava_public_subnet.*.id}"
+output "apm_public_subnets" {
+    value = "${aws_subnet.apm_public_subnet.*.id}"
 }
 
-output "endava_private_subnets" {
-    value = "${aws_subnet.endava_private_subnet.*.id}"
+output "apm_private_subnets" {
+    value = "${aws_subnet.apm_private_subnet.*.id}"
 }
 
-output "endava_private_sg" {
-    value = "${aws_security_group.endava_private_sg.id}"
+output "apm_private_sg" {
+    value = "${aws_security_group.apm_private_sg.id}"
 }
 
-output "endava_public_sg" {
-    value = "${aws_security_group.endava_public_sg.id}"
+output "apm_public_sg" {
+    value = "${aws_security_group.apm_public_sg.id}"
 }
 
-output "endava_alb_sg" {
-    value = "${aws_security_group.endava_alb_sg.id}"
+output "apm_alb_sg" {
+    value = "${aws_security_group.apm_alb_sg.id}"
 }
 
 output "vpc_id" {
-    value = "${aws_vpc.endava_vpc.id}"
+    value = "${aws_vpc.apm_vpc.id}"
 }
