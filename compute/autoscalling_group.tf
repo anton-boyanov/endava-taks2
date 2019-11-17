45e646932228b5e25edf3048f935456c3edd8ad9
anton-boyanov <hmmmmm75@yahoo.com>
2019-11-17 05:39:37 +0200

    apm

--- compute/autoscalling_group.tf
+++ compute/autoscalling_group.tf
@@ -5,8 +5,8 @@ resource "aws_launch_configuration" "webapp_on_demand" {
     image_id = "${var.ecs_image_id}"
     iam_instance_profile = "${var.ecs_instance_profile}"
     user_data = "${data.template_file.autoscaling_user_data.rendered}"
-    key_name = "${aws_key_pair.endava_auth.id}"
-    security_groups = ["${var.endava_public_sg}"]
+    key_name = "${aws_key_pair.apm_auth.id}"
+    security_groups = ["${var.apm_public_sg}"]
     associate_public_ip_address = true
 
     lifecycle {
@@ -15,7 +15,7 @@ resource "aws_launch_configuration" "webapp_on_demand" {
 }
 
 resource "aws_autoscaling_group" "webapp_on_demand" {
-    name = "endava_webapp_on_demand"
+    name = "apm_webapp_on_demand"
     max_size = 50
     min_size = 0
     desired_capacity = "${var.desired_capacity_on_demand}"
@@ -23,11 +23,11 @@ resource "aws_autoscaling_group" "webapp_on_demand" {
     health_check_type = "EC2"
     force_delete = true
     launch_configuration = "${aws_launch_configuration.webapp_on_demand.name}"
-    vpc_zone_identifier = ["${var.endava_public_subnets}"]
+    vpc_zone_identifier = ["${var.apm_public_subnets}"]
 
     tag {
         key = "Name"
-        value = "endava"
+        value = "apm"
         propagate_at_launch = true
     }
 
