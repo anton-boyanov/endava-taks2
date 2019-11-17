d4b704f677d8a05b244fe912fb8d0953679f21ea
anton-boyanov <hmmmmm75@yahoo.com>
2019-11-17 04:58:21 +0200

    Update variables.tf
    changed cidrs

--- networking/variables.tf
+++ networking/variables.tf
@@ -1,17 +1,17 @@
 #----networking/variables.tf
 
 variable "vpc_cidr" {
-    default = "10.0.0.0/16"
+    default = "10.2.0.0/16"
 }
 
 variable "public_cidrs" {
     type = "list"
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+    default = ["10.2.1.0/24", "10.2.2.0/24"]
 }
 
 variable "private_cidrs" {
     type = "list"
-    default = ["10.0.3.0/24", "10.0.4.0/24"]
+    default = ["10.2.3.0/24", "10.2.4.0/24"]
 }
 
 variable "accessip" {
